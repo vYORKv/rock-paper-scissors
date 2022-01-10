8ec06e61eaac4482a9575326fd88c2486439cf14
vYORKv <vYORKv@gmail.com>
2022-01-10 11:45:02 -0600

    Finished full gameplay loop and endGame function

--- rps-script.js
+++ rps-script.js
@@ -1,91 +1,83 @@
 let compScore = 0;
 let playerScore = 0;
 
-//Randomly generates computer choice
+// Randomly generates computer choice
 function computerPlay() {
-    let options = ["rock", "paper", "scissors"];
+    let options = ["Rock", "Paper", "Scissors"];
     let choice = options[Math.floor(Math.random() * options.length)];
     return choice;
 }
 
-//Takes player input from button and returns it for playOnce function
+// Takes player input from button and returns it for playOnce function
 function playerPlay(playerInput) {
-    // let playerInput = prompt("Please enter a selection:").trim().toLowerCase();
     return playerInput;
 }
 
-//Single playthrough pitting player against computer
-function playOnce(playerInput) {
+// Playthrough pitting player against computer and updates scores based on results
+function playGame(playerInput) {
     let pi = playerInput
     let pc = playerPlay(pi);
     let cc = computerPlay();
-    if ((pc == "rock" && cc == "paper") || (pc == "paper" && cc == "scissors") || (pc == "scissors" && cc == "rock"))
+    
+    if ((pc == "Rock" && cc == "Paper") || (pc == "Paper" && cc == "Scissors") || (pc == "Scissors" && cc == "Rock"))
      {
-         alert(`You chose: ${pc} and Computer chose: ${cc}`);
          compScore++;
          cscore.textContent = `Computer Score: ${compScore}`;
-     }
-     else if ((pc == "rock" && cc == "scissors") || (pc == "scissors" && cc == "paper") || (pc == "paper" && cc == "rock"))
-     {
          alert(`You chose: ${pc} and Computer chose: ${cc}`);
+     }
+     else if ((pc == "Rock" && cc == "Scissors") || (pc == "Scissors" && cc == "Paper") || (pc == "Paper" && cc == "Rock"))
+     {
          playerScore++;
          pscore.textContent = `Your Score: ${playerScore}`;
+         alert(`You chose: ${pc} and Computer chose: ${cc}`);
      }
      else if (pc == cc)
      {
          alert(`You chose: ${pc} and Computer chose: ${cc}`);
      }
+    endGame();
 }
 
-/* function updateScores() {
-    return 
-} */
-
-//Function that loops the game five times and outputs winner based on final score
-/* function game() {
-    for (let i = 0; i <= 4; i++) {
-        playOnce();
+// Ends the game when either player or computer reaches 5 points
+function endGame() {
+    if (playerScore == 5) {
+        alert("Congratulations! You win! Press okay to play again!");
+        playerScore = 0;
+        compScore = 0;
+        pscore.textContent = `Your Score: ${playerScore}`;
+        cscore.textContent = `Computer Score: ${compScore}`;
     }
-    if (playerScore < compScore)
-     {
-        console.log(`The final score is ${playerScore}-${compScore}. You lose the match!`);
+     else if (compScore == 5) {
+        alert("Oh no! You lose! Press okay to play again!");
+        playerScore = 0;
+        compScore = 0;
+        pscore.textContent = `Your Score: ${playerScore}`;
+        cscore.textContent = `Computer Score: ${compScore}`;
      }
-     else if (playerScore > compScore)
-     {
-        console.log(`The final score is ${playerScore}-${compScore}. You win the match!`);
+     else {
+         return;
      }
-     else
-     {
-        console.log(`The final score is ${playerScore}-${compScore}. The match results in a tie!`);
-     }
-} */
+}
 
-/* function outputEven() {
-    for (let i = 1; i <= 10; i++) {
-        if (i % 2 == 0)
-        {
-            console.log(i);
-        }
-    }
-} */
-
-//JS-CSS Script
-const rockbtn = document.querySelector("#rock");
-const paperbtn = document.querySelector("#paper");
-const scissorsbtn = document.querySelector("#scissors");
+//JS-CSS/HTML Script
+const Rockbtn = document.querySelector("#Rock");
+const Paperbtn = document.querySelector("#Paper");
+const Scissorsbtn = document.querySelector("#Scissors");
 const pscore = document.querySelector("#pscore");
 const cscore = document.querySelector("#cscore");
+const body = document.body;
+const h1 = document.createElement("h1");
+
+h1.textContent = "Rock Paper Scissors! First to 5 wins!";
+body.insertBefore(h1, Rockbtn);
 
 
-rockbtn.addEventListener("click", () => {
-    playOnce("rock");
+Rockbtn.addEventListener("click", () => {
+    playGame("Rock");
 })
-paperbtn.addEventListener("click", () => {
-    playOnce("paper");
+Paperbtn.addEventListener("click", () => {
+    playGame("Paper");
 })
-scissorsbtn.addEventListener("click", () => {
-    playOnce("scissors");
+Scissorsbtn.addEventListener("click", () => {
+    playGame("Scissors");
 })
-/* const scores = document.createElement("h1");
-scores.textContent = `Score: You - ${playerScore}  Computer - ${compScore}`;
-scoresdiv.appendChild(scores); */
